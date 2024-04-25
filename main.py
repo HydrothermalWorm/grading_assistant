a0246b89fdcc048aa6ebcffb39685a591102395c
HydrothermalWorm <jakekellermeier@gmail.com>
2024-04-25 04:38:08 -0400

    update

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from assets import GradingAssistant, rubric
 
 
 def main():
-    rubric_data = rubric("assets/rubric.json")
-    grade = GradingAssistant(rubric_data)
-    grade.grade()
-    grade.summarize()
-
+    rubric_data = rubric("assets/sep_of_proteins_2_3.json")
+    assistant = GradingAssistant(rubric_data)
+    assistant.iterate_over_dictionary(assistant.rubric_dict)
+    assistant.summarize()
+    # assistant.debug()
     
 if __name__ == "__main__":
     main()
